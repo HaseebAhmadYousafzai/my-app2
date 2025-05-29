4e805dc5ce89e1dd4d17fa6a648064f2031af90c
HaseebAhmadYousafzai <221466@students.au.edu.pk>
2025-05-29 15:14:31 +0500

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello world heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/hello world/i);
+  expect(heading).toBeInTheDocument();
 });
